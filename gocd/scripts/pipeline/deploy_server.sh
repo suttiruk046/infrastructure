419fc24aa6e20f42de7e154159dffb6211638efb
suttiruk046 <suttiruk046@yahoo.com>
2017-09-30 18:06:19 +0700

    gocd/scripts/pipeline/deploy_server.sh fix wrong dc name

--- gocd/scripts/pipeline/deploy_server.sh
+++ gocd/scripts/pipeline/deploy_server.sh
@@ -7,5 +7,5 @@ oc start-build bc/gocd-server \
                --namespace=${oc_namespace}
 
 oc rollout latest dc/gocd-server --namespace=${oc_namespace} && \
-oc logs -f dc/api-gocd-server --namespace=${oc_namespace}
+oc logs -f dc/gocd-server --namespace=${oc_namespace}
 oc rollout status dc/gocd-server --namespace=${oc_namespace}

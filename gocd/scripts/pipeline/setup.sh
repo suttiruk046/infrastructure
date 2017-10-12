ff20b727d5046fdf83f3d2211785550b77286d10
Suttiruk Krutnoy <suttiruk.kru@krungthai-axa.co.th>
2017-10-12 17:00:24 +0700

    gocd: use apply instead of replace

--- gocd/scripts/pipeline/setup.sh
+++ gocd/scripts/pipeline/setup.sh
@@ -5,17 +5,17 @@ set -eu
 # Cleanup old builds (in case the build hangs)
 oc delete --namespace=${oc_namespace} --cascade=true bc/gocd-server || true
 
-oc replace --namespace=${oc_namespace} -f gocd/openshift/common/configmap-infrastructure-gocd.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/common/serviceaccount-infrastructure-gocd.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/common/rolebiding-infrastructure-gocd.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/common/configmap-infrastructure-gocd.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/common/serviceaccount-infrastructure-gocd.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/common/rolebiding-infrastructure-gocd.yaml
 
-oc replace --force --namespace=${oc_namespace} -f gocd/openshift/agent/buildconfig-infrastructure-gocd-agent.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/agent/deploymentconfig-infrastructure-gocd-agent.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/agent/imagestream-infrastructure-gocd-agent.yaml
+oc apply --force --namespace=${oc_namespace} -f gocd/openshift/agent/buildconfig-infrastructure-gocd-agent.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/agent/deploymentconfig-infrastructure-gocd-agent.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/agent/imagestream-infrastructure-gocd-agent.yaml
 
-oc replace --force --namespace=${oc_namespace} -f gocd/openshift/server/buildconfig-infrastructure-gocd-server.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/server/deploymentconfig-infrastructure-gocd-server.yaml
-oc replace --namespace=${oc_namespace} -f gocd/openshift/server/imagestream-infrastructure-gocd-server.yaml
+oc apply --force --namespace=${oc_namespace} -f gocd/openshift/server/buildconfig-infrastructure-gocd-server.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/server/deploymentconfig-infrastructure-gocd-server.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/server/imagestream-infrastructure-gocd-server.yaml
 oc create  --namespace=${oc_namespace} -f gocd/openshift/server/persistentvolumeclaim-infrastructure-gocd-server.yaml || true
-oc replace --namespace=${oc_namespace} -f gocd/openshift/server/route-infrastructure-gocd-server.yaml
+oc apply --namespace=${oc_namespace} -f gocd/openshift/server/route-infrastructure-gocd-server.yaml
 oc apply --namespace=${oc_namespace} -f gocd/openshift/server/service-infrastructure-gocd-server.yaml
